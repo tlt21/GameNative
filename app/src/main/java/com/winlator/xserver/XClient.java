6c0dd0694de3e2436a948526b0d51c134759736c
xXJSONDeruloXx <danielhimebauch@gmail.com>
2026-04-03 09:18:00 -0400

    fix: sendEvent stream lock, capturedGen ordering

--- app/src/main/java/com/winlator/xserver/XClient.java
+++ app/src/main/java/com/winlator/xserver/XClient.java
@@ -4,6 +4,7 @@ import androidx.collection.ArrayMap;
 
 import com.winlator.xconnector.XInputStream;
 import com.winlator.xconnector.XOutputStream;
+import com.winlator.xconnector.XStreamLock;
 import com.winlator.xserver.events.Event;
 import com.winlator.xserver.extensions.XInput2Extension;
 
@@ -51,7 +52,7 @@ public class XClient implements XResourceManager.OnResourceLifecycleListener {
     }
 
     public void sendEvent(Event event) {
-        try {
+        try (XStreamLock ignored = outputStream.lock()) {
             event.send(sequenceNumber, outputStream);
         }
         catch (IOException e) {
